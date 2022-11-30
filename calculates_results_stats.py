5bfb4ff68f52b5d809eca3d197906b64c50f0e13
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2022-11-30 12:25:40 +0530

    Update
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -42,3 +42,41 @@
 #       in the return statement with the results_stats_dic dictionary that you create 
 #       with this function
 # 
+def calculates_results_stats(results_dic):
+    # Initializing empty dict
+    results_stats_dict: dict = {}
+    
+    for key,val in results_dic.items():
+        results_stats_dict[key] = 0
+    
+    for ky,val in result_dic.items():
+        results_stats_dict[key] += val[4]
+        
+    """
+    Calculates statistics of the results of the program run using classifier's model 
+    architecture to classifying pet images. Then puts the results statistics in a 
+    dictionary (results_stats_dic) so that it's returned for printing as to help
+    the user to determine the 'best' model for classifying images. Note that 
+    the statistics calculated as the results are either percentages or counts.
+    Parameters:
+      results_dic - Dictionary with key as image filename and value as a List 
+             (index)idx 0 = pet image label (string)
+                    idx 1 = classifier label (string)
+                    idx 2 = 1/0 (int)  where 1 = match between pet image and 
+                            classifer labels and 0 = no match between labels
+                    idx 3 = 1/0 (int)  where 1 = pet image 'is-a' dog and 
+                            0 = pet Image 'is-NOT-a' dog. 
+                    idx 4 = 1/0 (int)  where 1 = Classifier classifies image 
+                            'as-a' dog and 0 = Classifier classifies image  
+                            'as-NOT-a' dog.
+    Returns:
+     results_stats_dic - Dictionary that contains the results statistics (either
+                    a percentage or a count) where the key is the statistic's 
+                     name (starting with 'pct' for percentage or 'n' for count)
+                     and the value is the statistic's value. See comments above
+                     and the previous topic Calculating Results in the class for details
+                     on how to calculate the counts and statistics.
+    """        
+    # Replace None with the results_stats_dic dictionary that you created with 
+    # this function 
+    return results_stats_dic
